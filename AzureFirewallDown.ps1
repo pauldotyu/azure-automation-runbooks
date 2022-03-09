be977e7b9d0af32e57d0e2aba73ea54f72d256f7
Paul Yu <45927715+pauldotyu@users.noreply.github.com>
2022-03-09 07:14:32 -0800

    Update AzureFirewallDown.ps1

--- AzureFirewallDown.ps1
+++ AzureFirewallDown.ps1
@@ -22,7 +22,7 @@ catch {
     throw [System.Exception]::new('Failed to authenticate Azure using System-Assigned Managed Identity', $PSItem.Exception)
 }
 
-Write-Log "Successfully authenticated with Azure using System-Assigned Managed Identity: $($AzContext | Format-List -Force | Out-String)"
+Write-Output "Successfully authenticated with Azure using System-Assigned Managed Identity: $($AzContext | Format-List -Force | Out-String)"
 
 # Deallocate
 $firewall=Get-AzFirewall -ResourceGroupName $rgname -Name $fwname
